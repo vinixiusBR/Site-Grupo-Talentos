50673180b580feb1ac88c2c61820559913e5bd78
Marcos.silva <marcosilva.vinicius09@gmail.com>
2024-11-29 10:13:51 -0300

    Criação da pagina Trabalhe conosco iniciada e link âncoras implementados

---
+++ trabalheconosco.js
@@ -0,0 +1,21 @@
+const botaomenu = document.getElementById("botaomenu");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+
+botaomenu.addEventListener("click", function() {
+    menu.classList.toggle("show");
+    
+    overlay.classList.toggle("show");
+   
+    botaomenu.classList.toggle("active");
+
+   
+    const icon = botaomenu.querySelector(".iconify");
+
+    
+    if (icon.getAttribute("data-icon") === "codicon:three-bars") {
+        icon.setAttribute("data-icon", "material-symbols:close");
+    } else {
+        icon.setAttribute("data-icon", "codicon:three-bars");
+    }
+});
