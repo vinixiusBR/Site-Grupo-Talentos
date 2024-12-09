15f8dedf28088a09d98b8bf8e7efe93324d67c2a
Marcos.silva <marcosilva.vinicius09@gmail.com>
2024-12-09 12:47:01 -0300

    efeitos do header mobile adicionados

--- politicaeseguranca.js
+++ politicaeseguranca.js
@@ -32,11 +32,50 @@ acordos.forEach(button =>{
         
         const panel = this.nextElementSibling;
 
+
+        if (panel.style.height === '0px' || panel.style.height === '') {
+            panel.style.display = 'block';
+            panel.style.height = '0';
+            panel.style.opacity = '0';
+
+            setTimeout(() => {
+
+                panel.style.transition = 'height 1s ease-out, opacity 1s ease-out';
+                panel.style.height = 'auto';
+                panel.style.opacity = '1';
+            }, 10); 
+        } else {
+            
+            panel.style.height = '0';
+            panel.style.opacity = '0';
+
+            setTimeout(() => {
+                
+
+                panel.style.display = 'none';
+                panel.style.transition = 'height 1s ease-out, opacity 1s ease-out'
+                
+            }, 10);
+
+            
+
+    }
+    
+
+
+
+
+
+
+
         panel.classList.toggle('active');
+        
 
         if (imagem.style.display === "none") {
 
             imagem.style.display = "block";
+            imagem.style.transition = "all 0.5s ease";
+
 
         } else {
             imagem.style.display = "none";
